d4bf1b90bd1b3a21e68b129c3f370ae33c385e89
khurananick <khurananick@gmail.com>
2020-05-01 23:33:58 -0400

    Update flex_to_outbound_sms.js

--- flex_to_outbound_sms.js
+++ flex_to_outbound_sms.js
@@ -1,4 +1,14 @@
 exports.handler = async function(context, event, callback) {
+  const response        = new Twilio.Response();
+  response.appendHeader('Content-Type', 'application/json');
+  response.appendHeader('Access-Control-Allow-Origin', '*');
+
+  const errorHandler    = function(err) {
+    response.setBody(err);
+    callback(err, null);
+    process.exit(1);
+  };
+
   const client          = require("twilio")(process.env.TWILIO_FLEX_ACCOUNT_SID, process.env.TWILIO_FLEX_AUTH_TOKEN);
   const contactName     = event.ToName;
   const contactNumber   = event.ToNumber;
@@ -32,7 +42,7 @@ exports.handler = async function(context, event, callback) {
          longLived: true,
          janitorEnabled: true
        })
-      .catch(function(err) { console.log(err); })
+      .catch(errorHandler)
 
   // create a channel for this outbound number
   const newChannel = await client.flexApi.channel
@@ -51,14 +61,16 @@ exports.handler = async function(context, event, callback) {
        flexFlowSid: flexFlow.sid,
        chatUserFriendlyName: contactName
      })
-    .catch(function(err) { console.log(err); });
+    .catch(errorHandler)
 
   /* re-use long-lived sessions. */
   const proxySessions = await client.proxy.services(proxyServiceSid).sessions.list();
   if(proxySessions)
     for(let session of proxySessions)
-      if(session.uniqueName == newChannel.sid)
-        return callback(null, {success: true})
+      if(session.uniqueName == newChannel.sid) {
+        response.setBody({success: true});
+        return callback(null, response);
+      }
 
   // if no proxy session exists, create one and assign users to it
   const proxySession = await client.proxy.services(proxyServiceSid)
@@ -67,7 +79,7 @@ exports.handler = async function(context, event, callback) {
        uniqueName: newChannel.sid,
        mode: 'message-only'
      })
-    .catch(function(err) { console.log(err); });
+    .catch(errorHandler)
 
   // add agent as participant 1
   const insider = await client.proxy.services(proxyServiceSid)
@@ -80,7 +92,8 @@ exports.handler = async function(context, event, callback) {
     .sessions(proxySession.sid)
     .participants
     .create({friendlyName: contactName, identifier: contactNumber, proxyIdentifier: flexPhoneNum})
-    .catch(function(err) { console.log(err); });
+    .catch(errorHandler)
 
+  response.setBody({success: true});
   callback(null, {success: true});
 }
